288dbff9c6a573ac843b9c3363d0ffac773e2088
Prasaddighe2005 <126890225+Prasaddighe2005@users.noreply.github.com>
2023-03-29 17:35:20 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text('hello world',
+      textDirection: TextDirection.ltr,
+      ),
+     
+    ),
+  );
+}
