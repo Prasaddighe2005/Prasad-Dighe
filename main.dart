597f57afccccb4177a1470b0577399d3d930348a
Prasaddighe2005 <126890225+Prasaddighe2005@users.noreply.github.com>
2023-03-31 14:25:34 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text('hello world',
-      textDirection: TextDirection.ltr,
-      ),
-     
+    MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+     title: Text('New Creation'),
     ),
-  );
+    body: Center(child: 
+    Container(
+      height: 100,
+      width: 80,
+      color: Colors.orange,
+       
+    )
+    ,)
+    )));
+  
 }
